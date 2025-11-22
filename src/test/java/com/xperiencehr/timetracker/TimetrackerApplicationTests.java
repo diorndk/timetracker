3830afa21a4cce865081dd784180bfca0fde1414
Dio Riandika <dio.rndk@gmail.com>
2025-11-22 19:58:40 +0700

    Setup project and dependencies

---
+++ src/test/java/com/xperiencehr/timetracker/TimetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xperiencehr.timetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
